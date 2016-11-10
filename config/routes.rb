575c38cc8cd25b9f510cb20f6ba4419339cf46cc
martin byrne <marb61a@gmail.com>
2016-11-10 18:51:02 +0000

    added basic footer partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+  get 'pages/about', controller: 'pages', action: 'about'
+  get 'pages/contact', controller: 'pages', action: 'contact'
 
   root to: 'restaurants#index'
-
-  devise_for :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
