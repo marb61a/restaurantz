8a297605e861ad80d2ba77b1c19749af387d6ead
martin byrne <marb61a@gmail.com>
2016-11-16 19:08:43 +0000

    updated restaurants controller and show view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,26 @@
 class RestaurantsController < ApplicationController
+  before_action :set_user, only: [:index, :create, :update, :destroy]
+  
   def index
   end
+  
+  def new
+    @restaurant = Restaurant.new
+  end
+  
+  def create
+  end
+  
+  def show
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+    def restaurant_params
+    end
 end
