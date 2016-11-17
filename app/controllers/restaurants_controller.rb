1ada001d9a0da0ee0189a79716b2c961f12bb0e9
martin byrne <marb61a@gmail.com>
2016-11-17 19:29:56 +0000

    updated restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,9 +9,25 @@ class RestaurantsController < ApplicationController
   end
   
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant
+      flash[:success] = ""
+    else
+      flash[:error] = ""
+      render :new
+    end
   end
   
   def show
+    @user = current_user
+  end
+  
+  def search
+    @restaurants = Restaurant.search(params)
+  end
+  
+  def edit
   end
   
   def update
@@ -21,6 +37,12 @@ class RestaurantsController < ApplicationController
   end
   
   private
+    def set_user
+      @user = current_user
+    end
+    
     def restaurant_params
+      params.require(:restaurant).permit(:name, :description, :address1, :address2,
+      :city, :state_provence, :postalcode, :phone, :email, :category_id, :image)
     end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,12 @@
 class Restaurant < ApplicationRecord
     has_many :reviews
     belongs_to :category
+    validates_presence_of :name, :description, :city, :state_provence
+    
+    def full_address
+    end
+    
+    def self.search(params)
+    end
     
 end
