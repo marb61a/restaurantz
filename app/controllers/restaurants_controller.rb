19d783ad291dd5ff117fbaf1f031de645ace6aee
martin byrne <marb61a@gmail.com>
2016-11-21 19:31:13 +0000

    updated restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,8 @@
 class RestaurantsController < ApplicationController
+  before_action :admin?, only: [:edit, :update, :destroy,]
   before_action :set_user, only: [:index, :create, :update, :destroy]
+  before_action :set_the_id, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create] 
   
   def index
   end
@@ -37,8 +40,20 @@ class RestaurantsController < ApplicationController
   end
   
   private
+  
+    def admin?
+      if !current_user.try(:admin?)
+        flash[:danger] = "You are not authourized to access this resource."
+        redirect_to root_path
+      end
+    end 
+    
     def set_user
       @user = current_user
+    end
+    
+    def set_the_id
+      @restaurant = Restaurant.find(params[:id])
     end
     
     def restaurant_params
