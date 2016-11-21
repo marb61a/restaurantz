05f4c3fcc28240acbe8fefd36fe01602a4bd5c98
martin byrne <marb61a@gmail.com>
2016-11-21 19:20:12 +0000

    added migration and updated show restaurant view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161031142956) do
+ActiveRecord::Schema.define(version: 20161121185856) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20161031142956) do
     t.string   "email"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_restaurants_on_category_id"
   end
 
   create_table "reviews", force: :cascade do |t|
