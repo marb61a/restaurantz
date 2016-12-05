4931687c5084d950eee1fbc302bd5765966b1ea6
martin byrne <marb61a@gmail.com>
2016-12-05 18:28:43 +0000

    added form to restaurant index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,18 @@
 class PagesController < ApplicationController
+  before_action :set_page, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update]
+  
+  # GET /pages
+  # GET /pages.json
   def index
+    @page = Page.find(1)
+  end
+  
+  def about
+    
+  end
+  
+  def contact
+    
   end
 end
